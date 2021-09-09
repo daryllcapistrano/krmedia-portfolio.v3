d16228a57bcc9d0351b6fc91117804fc0a393dba
Daryll Capistrano <dcapistrano.dev@gmail.com>
2021-09-09 02:19:58 -0700

    add frontmatter to mdx files

--- src/pages/videography/index.js
+++ src/pages/videography/index.js
@@ -1,42 +1,36 @@
 import * as React from "react"
+import { graphql } from "gatsby"
 import Layout from "../../components/Layout"
 import { SimpleGrid, Box } from "@chakra-ui/react"
 
-const VideographyPage = () => {
+const VideographyPage = ({data}) => {
   return (
     <main>
       <Layout>
         <SimpleGrid columns={[1, null, 2]} spacing="20px" margin="10px">
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
-          <Box bg="orange" height="333px"></Box>
+          {
+            data.allFile.nodes.map(node => (
+              <Box bg="orange" height="333px">{node.name}</Box>
+            ))
+          }
+         
           
         </SimpleGrid>
       </Layout>
     </main>
   )
 }
+
+export const query = graphql`
+  {
+    allFile(filter: {sourceInstanceName: {eq: "markdown"}}) {
+      nodes {
+        id
+        name
+      }
+    }
+  }
+`
 
 export default VideographyPage
 
